d05e950e77f7076f6f787ee223a0092e2526aeb6
tommy-cheng <tommy.cfc.cheng@gmail.com>
2015-04-01 14:41:57 +0800

    Chapter 8 - 0  P.152

--- app/src/main/java/com/example/tommy/criminalintent/Crime.java
+++ app/src/main/java/com/example/tommy/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.tommy.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,10 +10,14 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
 
     public Crime() {
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getmId() {
@@ -27,4 +32,19 @@ public class Crime {
         this.mTitle = mTitle;
     }
 
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSolved() {
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
 }
