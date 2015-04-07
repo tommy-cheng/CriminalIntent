6cdf36aadea06a5d8d7645334be3d4a9f7c8f599
tommy-cheng <tommy.cfc.cheng@gmail.com>
2015-04-07 17:28:50 +0800

    Chapter 10-1, P.196

--- app/src/main/java/com/example/tommy/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/tommy/criminalintent/CrimeActivity.java
@@ -2,10 +2,14 @@ package com.example.tommy.criminalintent;
 
 import android.support.v4.app.FragmentActivity;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected android.support.v4.app.Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID)getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 }
--- app/src/main/java/com/example/tommy/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/tommy/criminalintent/CrimeFragment.java
@@ -32,7 +32,7 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        UUID crimeId = (UUID)getActivity().getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
 
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
@@ -77,5 +77,16 @@ public class CrimeFragment extends Fragment {
         return v;
 
     }
+
+    public static CrimeFragment newInstance(UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_CRIME_ID, crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
 }
+
 
