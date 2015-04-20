34045792195adc27eac66559418c83c2be40ec8f
tommy-cheng <tommy.cfc.cheng@gmail.com>
2015-04-20 17:30:23 +0800

    Chapter 19-0, 1st Camera application, P.315

---
+++ app/src/main/java/com/example/tommy/criminalintent/CrimeCameraActivity.java
@@ -0,0 +1,13 @@
+package com.example.tommy.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by tommy on 4/17/2015.
+ */
+public class CrimeCameraActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeCameraFragment();
+    }
+}
--- app/src/main/java/com/example/tommy/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/tommy/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -19,6 +20,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.hardware.Camera;
 
 import java.text.DateFormat;
 import java.text.FieldPosition;
@@ -39,6 +42,7 @@ public class CrimeFragment extends Fragment {
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
+    private ImageButton mPhotoButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -123,8 +127,27 @@ public class CrimeFragment extends Fragment {
                 mCrime.setmSolved(isChecked);
             }
         });
-        return v;
 
+
+        mPhotoButton = (ImageButton)v.findViewById(R.id.crime_imageButton);
+        mPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent i = new Intent(getActivity(), CrimeCameraActivity.class);
+               startActivity(i);
+            }
+        });
+
+        // If camera is not available, disable camera functionality
+        PackageManager pm = getActivity().getPackageManager();
+        boolean hasACamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA) ||
+                pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT) ||
+                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD &&
+                Camera.getNumberOfCameras() > 0);
+        if (!hasACamera) {
+            mPhotoButton.setEnabled(false);
+        }
+        return v;
     }
 
     @Override
