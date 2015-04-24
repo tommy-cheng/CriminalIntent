5a1b505c94cfdd121aebd7eeb24f3bc196daf1c2
tommy-cheng <tommy.cfc.cheng@gmail.com>
2015-04-24 16:17:37 +0800

    Chapter 22-0 - P364

--- app/src/main/java/com/example/tommy/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/tommy/criminalintent/SingleFragmentActivity.java
@@ -11,10 +11,14 @@ import android.support.v4.app.FragmentActivity;
 public abstract class SingleFragmentActivity extends FragmentActivity {
     protected abstract Fragment createFragment();
 
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
 
